d8fa401471adbcc6a370758d60d6c836b5b3228b
yu120 <595208882@qq.com>
2017-07-29 14:54:30 +0800

    删除冗余代码

--- mconf/src/main/java/cn/ms/mconf/support/AbstractMconf.java
+++ mconf/src/main/java/cn/ms/mconf/support/AbstractMconf.java
@@ -1,9 +1,5 @@
 package cn.ms.mconf.support;
 
-import java.util.Map;
-
-import org.apache.commons.beanutils.BeanUtils;
-import org.jboss.netty.util.internal.ConcurrentHashMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,22 +16,10 @@ public abstract class AbstractMconf implements Mconf {
 	public static final String DEFAULT_KEY = "default_";
 
 	protected String ROOT;
-	protected Category category = new Category();
-	protected Map<String, String[]> structureMap = new ConcurrentHashMap<String, String[]>();
 
 	@Override
 	public void connect(URL url) {
 		this.ROOT = url.getPath();
-		this.structureMap.put(Cmd.ROOT_KEY, url.getParameter(Cmd.ROOT_KEY, new String[] {}));
-		this.structureMap.put(Cmd.APP_KEY, url.getParameter(Cmd.APP_KEY, new String[] {}));
-		this.structureMap.put(Cmd.CONF_KEY, url.getParameter(Cmd.CONF_KEY, new String[] {}));
-		this.structureMap.put(Cmd.DATA_KEY, url.getParameter(Cmd.DATA_KEY, new String[] {}));
-
-		try {
-			BeanUtils.copyProperties(category, url.getParameters());
-		} catch (Exception e) {
-			logger.error("The copyProperties exception.", e);
-		}
 	}
 
 	@SuppressWarnings("unchecked")
